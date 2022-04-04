f864f3bfff1c6657895665d25403e30e6319bbce
Marco Castope <marcocastope7@gmail.com>
2022-04-03 21:14:29 -0500

    se agrega el detail viewmodel al app module para inyectar en el fragment detail

--- app/src/main/java/com/marcocastope/moviesapp/di/AppModule.kt
+++ app/src/main/java/com/marcocastope/moviesapp/di/AppModule.kt
@@ -3,6 +3,8 @@ package com.marcocastope.moviesapp.di
 import com.marcocastope.moviesapp.data.sources.impl.RepositoryImpl
 import com.marcocastope.moviesapp.domain.repository.Repository
 import com.marcocastope.moviesapp.domain.usecases.GetAllMoviesUseCase
+import com.marcocastope.moviesapp.domain.usecases.GetMovieUseCase
+import com.marcocastope.moviesapp.presentation.viewmodels.DetailViewModel
 import com.marcocastope.moviesapp.presentation.viewmodels.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -12,6 +14,10 @@ val appModule = module {
     factory<Repository> { RepositoryImpl(get(), get()) }
 
     factory<GetAllMoviesUseCase> { GetAllMoviesUseCase(get()) }
+
+    factory { GetMovieUseCase(get()) }
+
+    viewModel { DetailViewModel(get()) }
 
     viewModel { HomeViewModel(get()) }
 }
